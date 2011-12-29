2e4ce301f9c7792c11f6380f267ee97b17a56b84
Jason Schroeder <jason@orangatame.com>
2011-12-28 20:05:25 -0800

    Fix typos in comments and remove some duplicated code from an unproof'd merge.

--- cloudcomfort-app.rb
+++ cloudcomfort-app.rb
@@ -79,19 +79,12 @@ end
 # From the Arduino:
 post '/api/'+ api_version + '/poll' do
 
-
-  # Temperature, celcius (number)
+  # Temperature, celsius (number)
   temp = params[:tempc].to_i
   logger.info "Receiving temperature report: '#{temp}'"
 
   settings.cache.set(:measured_temp, temp)
   
-
- vals = { :ac_power     => settings.cache.get(:ac_power),
-           :desired_temp => settings.cache.get(:desired_temp),
-           :fan_speed    => settings.cache.get(:fan_speed)}
-
-#  request.body.rewind # in case someone already read it
   vals = { :ac_power     => settings.cache.get(:ac_power),
            :desired_temp => settings.cache.get(:desired_temp),
            :fan_speed    => settings.cache.get(:fan_speed)}
@@ -106,12 +99,3 @@ def build_arduino_response(vals)
   return response
 end
 
-# Test at <appname>.heroku.com
-
-# You can see all your app specific information this way.
-# IMPORTANT! This is a very bad thing to do for a production
-# application with sensitive information
-
-# get '/env' do
-# ENV.inspect
-# end
